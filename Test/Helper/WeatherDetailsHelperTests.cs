926df87649d22e1d4363851e6506b4cfd709793c
Ramya Thankappan <Ramya.Thankappan@elders.com.au>
2023-05-03 15:51:08 +0930

    update Project structure

---
+++ Test/Helper/WeatherDetailsHelperTests.cs
@@ -0,0 +1,96 @@
+﻿using Xunit;
+using WeatherDetails.Helper;
+using WeatherDetails.Connector.Model;
+using System.Reflection;
+using System.Text.Json;
+using WeatherDetails.Model;
+
+namespace WeatherDetailsTests.Helper
+{
+    public  class WeatherDetailsHelperTests
+    {
+        [Theory]
+        [InlineData(100,212)]
+        [InlineData(0, 32)]
+        [InlineData(-10, 14)]
+        [InlineData(10.34, 50.612)]
+        public void ToFarenheit_ReturnsValue(double input, double expectedOutput)
+        {
+            var output = input.ToFarenheit();
+            Assert.Equal(expectedOutput, output);
+
+        }
+
+        [Theory]
+        [InlineData("0", true)]
+        [InlineData("390", false)]
+        [InlineData("-150", true)]
+        [InlineData("52.52", true)]
+
+        public void IsValidLongitude_ReturnsValue(string longitude, bool isValid)
+        {
+            var output = longitude.IsValidLongitude();
+            Assert.Equal(isValid, output);
+
+        }
+
+
+        [Theory]
+        [InlineData("0", true)]
+        [InlineData("200", false)]
+        [InlineData("-100", false)]
+        [InlineData("60", true)]
+        [InlineData("13.42", true)]
+        public void IsValidLatitude_ReturnsValue(string latitude, bool isValid)
+        {
+            var output = latitude.IsValidLatitude();
+            Assert.Equal(isValid, output);
+
+        }
+
+        [Fact]
+        public void ToResponse_ReturnsSuccess()
+        {
+            var expectedResponse = new WeatherDetailsResponse();
+
+            expectedResponse=  JsonSerializer.Deserialize<WeatherDetailsResponse>(ReadFromFile("WeatherDetailsTest.Inputs.weatherExport_20230503.json"));
+
+            var response = CreateMeteoResponse().ToResponse();
+            Assert.Equal(JsonSerializer.Serialize(expectedResponse), JsonSerializer.Serialize(response));
+        }
+
+        [Fact]
+        public void ToResponse_Returns_EmptySnowFall()
+        {
+            var expectedResponse = new WeatherDetailsResponse();
+
+            expectedResponse = JsonSerializer.Deserialize<WeatherDetailsResponse>(ReadFromFile("WeatherDetailsTest.Inputs.weatherExport_20230503.json"));
+
+            var response = CreateMeteoResponse().ToResponse();
+            Assert.Equal(JsonSerializer.Serialize(expectedResponse), JsonSerializer.Serialize(response));
+        }
+
+
+        private MeteoResponse CreateMeteoResponse()
+        {
+            return JsonSerializer.Deserialize<MeteoResponse>(ReadFromFile("WeatherDetailsTest.Inputs.Meteo_Success_EmptySnowfall.json"));
+        }
+
+
+
+        private string ReadFromFile(string resourceName)
+        {
+
+            var assembly = Assembly.GetExecutingAssembly();            
+            var result = string.Empty;
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                result = reader.ReadToEnd();
+            }
+
+            return result;
+
+        }
+    }
+}
---
+++ Test/WeatherConsoleAppTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using WeatherDetails;
+using WeatherDetails.Connector;
+
+namespace WeatherDetails.Tests
+{
+  
+        public class WeatherConsoleAppTests
+        {
+            private readonly IServiceProvider _serviceProvider;
+
+            public WeatherConsoleAppTests()
+            {
+                var serviceProvider = new Mock<IServiceProvider>();
+                serviceProvider.Setup(x => x.GetService(typeof(MeteoClient))).Returns(new MeteoClient(Mock.Of<HttpClient>()));
+
+                var serviceScope = new Mock<IServiceScope>();
+                serviceScope.Setup(x => x.ServiceProvider).Returns(serviceProvider.Object);
+
+                var serviceScopeFactory = new Mock<IServiceScopeFactory>();
+                serviceScopeFactory.Setup(x => x.CreateScope()).Returns(serviceScope.Object);
+
+                serviceProvider.Setup(x => x.GetService(typeof(IServiceScopeFactory))).Returns(serviceScopeFactory.Object);
+
+                _serviceProvider = serviceProvider.Object;
+            }
+
+            
+        }
+    }
